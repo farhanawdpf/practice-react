282b4e61fb59c360244d34b9768bec0431a8823a
farhanawdpf <farhanawdpf@gmail.com>
2024-06-13 18:39:46 +0600

    update

--- part-onepart-one/src/App.jsx
+++ part-onepart-one/src/App.jsx
@@ -1,7 +1,5 @@
 
 import React from 'react';
-import { Fragment } from 'react';
-import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -9,30 +7,35 @@ import {BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Blog from './pages/Blog';
-import Anotherlink from './pages/Anotherlink';
 import SecondPage from './pages/SecondPage';
+import Sidebar from './components/Sidebar';
+import Adduser from './pages/users/Adduser';
+import Manageuser from './pages/users/Manageuser';
+import Edituser from './pages/users/Edituser';
 
 
 const App = () => {
   return (
- <>
+<div className="wrapper">
      <BrowserRouter>
-     <Header/>
      <Navbar/>
+     <Sidebar/>
+     <div className='content-wrapper'> 
       <Routes>
-        <Route path="/" element={<Home />} >
-              <Route path="/anotherlink" element={<Anotherlink/>} />
-        </Route> 
+        <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact/>}/>
-
+        <Route path="/add-user" element={<Adduser/>}/>
+        <Route path="/manage-user" element={<Manageuser/>}/>
+        <Route path="/edit-user" element={<Edituser/>}/>
         <Route path="/blog" element={<Blog/>}>
            <Route path="second" element={<SecondPage/>}/>
         </Route>
       </Routes>
+      </div>
      <Footer/>
     </BrowserRouter>
 
-   </>
+   </div>
   );
 };
 
--- part-onepart-one/src/pages/Home.jsx
+++ part-onepart-one/src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import {Link, Outlet } from 'react-router-dom';
-
 const Home = () => {
     return (
-        <div>
-            <nav>
-            <Link to="/anotherlink" className="nav-link">My Profile</Link>
-            </nav>
-            <Outlet />
-        </div>
+    <div> 
+        <h1>Hello</h1>
+    </div>
+
     );
 };
 
